3c6da39590499ae75d500b9011e2c1ad115a7782
Kristine Lsx <lazdovska.kristine@gmail.com>
2021-09-01 17:56:10 +0300

    Initial Commit

---
+++ DemoSwiftUIConcept/DemoSwiftUIConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUIConceptApp.swift
+//  DemoSwiftUIConcept
+//
+//  Created by kristine.lazdovska on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUIConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
