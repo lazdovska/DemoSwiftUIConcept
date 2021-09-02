6ce9f012c703a5beee0eab6222ca3b27a795e80d
Kristine Lsx <lazdovska.kristine@gmail.com>
2021-09-02 13:23:36 +0300

    init made in Class

--- DemoSwiftUIConcept/ContentView.swift
+++ DemoSwiftUIConcept/ContentView.swift
@@ -8,12 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var circleColor = Color.red
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            Button("Change Circle Color") {
+                print("Change Circle Color")
+                self.circleColor = Color.green
+            }.padding()
+            .background(Color.yellow)
+            .cornerRadius(10)
+            .offset(y: -200)
+            
+            HStack{
+                Circle()
+                Circle()
+                Circle()
+                Circle()
+            }.foregroundColor(circleColor)
+            
+            VStack {
+                Text("Hello, girls!")
+                    .font(.caption2)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 100.0, height: 150.0)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+                
+                Text("Welcome to SwiftUI")
+                    .padding()
+                
+                Text("xcode lesson 14")
+                    .padding()
+                
+            }.font(.subheadline)
+            .foregroundColor(.blue)
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
